3b317f19c226173f4fd97a6cbf284c4d692e9624
Sirio Negri <ziriuz84@gmail.com>
2021-11-24 07:30:59 +0100

    added observatory_config_menu

--- asteroidpy/interface.py
+++ asteroidpy/interface.py
@@ -12,9 +12,34 @@
 # @section author_interface Author(s)
 # - Created by Sirio Negri on 11/23/2021
 # - modified by Sirio Negri on 11/23/2021
+import configuration
+
+
+def observatory_config_menu():
+    choice = 99
+    while (choice != 0):
+        print('Configurazione -> Osservatorio')
+        print('==============================')
+        print('\n')
+        configuration.print_obs_config()
+        print('Scegli un\'opzione')
+        print('1 - Cambia coordinate')
+        print('2 - Cambia altitudine')
+        print('Torna al menu della configurazione')
+        choice = eval(input('scelta -> '))
+        print('\n\n\n\n\n')
+        if choice == 1:
+            latitude = eval(input('Latitudine -> '))
+            longitude = eval(input('Longitudine -> '))
+            configuration.change_obs_coords(latitude, longitude)
+        if choice == 2:
+            altitude = eval(input('Altitudine -> '))
+            configuration.change_obs_altitude(altitude)
+
+
 def config_menu():
     choice = 99
-    while (choice!=0):
+    while (choice != 0):
         print('Configurazione')
         print('==============================')
         print('\n')
@@ -23,6 +48,7 @@ def config_menu():
         print('0 - Torna al menu principale')
         choice = eval(input('scelta -> '))
         print('\n\n\n\n\n')
+
 
 def main_menu():
     choice = 99
