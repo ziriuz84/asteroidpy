5a6a542da0c64c86af108e633395e7f9aab02837
Sirio Negri <ziriuz84@gmail.com>
2025-08-04 08:19:15 +0200

    chore(setup.py): better formatting

--- setup.py
+++ setup.py
@@ -1,27 +1,34 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='AsteroidPy',
-    version='0.1',
-    description='A tool for asteroid observation scheduling and analysis',
-    url='https://github.com/ziriuz84/asteroidpy',
-    author='Sirio Negri',
-    author_email='ziriuz84@gmail.com',
-    license='GPL v3',
-    packages=find_packages('.'),
-    install_requires=['requests',
-                      'bs4', 'configparser', 'astropy', 'httpx', 'astroplan', 'lxml', 'astroquery'],
-    classifiers=[
-        'Development Status :: 1 - Planning',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GPL v3',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 3',
+    name="AsteroidPy",
+    version="0.1",
+    description="A tool for asteroid observation scheduling and analysis",
+    url="https://github.com/ziriuz84/asteroidpy",
+    author="Sirio Negri",
+    author_email="ziriuz84@gmail.com",
+    license="GPL v3",
+    packages=find_packages("."),
+    install_requires=[
+        "requests",
+        "bs4",
+        "configparser",
+        "astropy",
+        "httpx",
+        "astroplan",
+        "lxml",
+        "astroquery",
+        "platformdirs",
     ],
-    py_modules=['configuration', 'scheduling', 'interface'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GPL v3",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+    ],
+    py_modules=["configuration", "scheduling", "interface"],
     entry_points={
-        'console_scripts': [
-            'asteroidpy=asteroidpy:main'
-        ],
+        "console_scripts": ["asteroidpy=asteroidpy:main"],
     },
 )
